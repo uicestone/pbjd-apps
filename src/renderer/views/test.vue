7baf99a4559495dbc55e6eaf9fe857975512d8a4
Uice Lu <uicestone@gmail.com>
2018-06-16 14:33:13 +0800

    Test index style

--- src/renderer/views/test.vue
+++ src/renderer/views/test.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
   div
     ul.menu-list
-      router-link(to="/home") 党建声音首页
-      router-link(to="/movie/index") 红色电影配音
-      router-link(to="/speak/index") 我要对党说..
-      router-link(to="/motto/index") 我的座右铭
-      router-link(to="/history") 往期精选  
-      router-link(to="/join/index") 加入
-      router-link(to="/map/index") 地图                                                      
+      li
+        router-link(to="/map/index") 党建地图
+      li
+        router-link(to="/home") 党建声音
+      li
+        router-link(to="/join/index") 历史上的那一天
 </template>
 
 <script>
@@ -15,8 +14,19 @@ export default {};
 </script>
 
 <style lang="stylus" scoped>
-.menu-list {
-  display: flex;
-  flex-direction: column;
-}
+.menu-list
+  display flex
+  flex-direction column
+  align-items center
+  justify-content space-around
+  height 50vh
+  margin-top 25vh
+  li
+    font-size 2vw
+    background #ce1f1f
+    color #0FF
+    padding 1vw 2vw
+    border-radius 1vw
+    a
+      color #fff3ae
 </style>
