c73f3828b9391b1f5bbfb18eeb686799d48e76cd
huangwei <huangwei@luk360.com>
2019-02-21 22:36:26 +0800

    首页图标变活

--- src/renderer/views/map/index.vue
+++ src/renderer/views/map/index.vue
@@ -13,10 +13,10 @@
       img.bg-element(:src="'static/images/slides/'+currentSlide+'.jpg'" @click="nextSlide")
       //- Icon.slide-close(type="ios-close-empty" v-if="currentSlide" @click="closeSlides")
     div.buttonGroup
-      img.glow(@click="showSlides" src="~@/assets//image/map_index_button1.png")
-      img.glow.delay-1(@click="goDetail" src="~@/assets//image/map_index_button2.png")
-      img.glow.delay-2(src="~@/assets//image/map_index_button3.png")
-      img.glow.delay-3(src="~@/assets//image/map_index_button4.png")
+      img.glow(@click="showSlides" :src="homeButtons[0]")
+      img.glow.delay-1(@click="goDetail" :src="homeButtons[1]")
+      img.glow.delay-2(:src="homeButtons[2]")
+      img.glow.delay-3(:src="homeButtons[3]")
     div.title-circle.fill(@click="showOpening")
     div.title-circle.no-2.fill.delay-1
     div.title-circle.no-3.fill.delay-2
@@ -35,10 +35,11 @@ export default {
   data() {
     return {
       isPlaying: false,
-      slideSections: ['1', '7', '16', '31'],
-      lastSlide: '32',
+      slideSections: ["1", "7", "16", "31"],
+      lastSlide: "32",
       currentSlide: null,
-      showingOpening: null
+      showingOpening: null,
+      homeButtons:[]
     };
   },
   watch: {
@@ -54,34 +55,35 @@ export default {
   },
   methods: {
     goDetail() {
+      console.log("godetail");
       this.$router.push({ name: "mapDetail" });
     },
     showOpening() {
       if (!this.showingOpening) {
-        this.showingOpening = 'image';
+        this.showingOpening = "image";
       } else {
         this.showingOpening = null;
         this.playVideo();
       }
     },
     playVideo() {
-      console.log('Play video.')
+      console.log("Play video.");
       this.isPlaying = true;
-      this.showingOpening = 'video'; // use video as opening
+      this.showingOpening = "video"; // use video as opening
     },
     videoEnded() {
-      console.log('Video ended.');
+      console.log("Video ended.");
       const { video } = this.$refs;
       // this.isPlaying = false;
     },
     closeVideo() {
-      console.log('Stop video.')
+      console.log("Stop video.");
       const { video } = this.$refs;
       this.isPlaying = false;
       this.showingOpening = null; // use video as opening
     },
     showSlides() {
-      this.currentSlide = '0';
+      this.currentSlide = "0";
     },
     nextSlide() {
       if (this.currentSlide === this.lastSlide) {
@@ -91,10 +93,10 @@ export default {
       }
     },
     backSlide() {
-      if (this.currentSlide === '0') {
+      if (this.currentSlide === "0") {
         this.closeSlides();
       } else {
-        this.currentSlide = '0';
+        this.currentSlide = "0";
       }
     },
     goToSlide(name) {
@@ -102,183 +104,276 @@ export default {
     },
     closeSlides() {
       this.currentSlide = null;
+    },
+    getSpotConfig() {
+      request.spotsConfig().then(res => {
+        console.log(res,'spotsconfig')
+        this.homeButtons = res.homeButtons;
+      });
     }
   },
   async mounted() {
     this.slides = await request.getMapSlides();
-    window.keyboardListener = (event) => {
-      if (event.key !== 'b') {
+    window.keyboardListener = event => {
+      if (event.key !== "b") {
         return;
       }
-      if (this.showingOpening === 'image') {
+      if (this.showingOpening === "image") {
         this.playVideo();
       } else {
         this.closeVideo();
       }
     };
-    window.addEventListener('keyup', window.keyboardListener);
+    window.addEventListener("keyup", window.keyboardListener);
+    this.getSpotConfig()
   },
   destroyed() {
-    window.removeEventListener('keyup', window.keyboardListener);
+    window.removeEventListener("keyup", window.keyboardListener);
   }
 };
 </script>
 
 <style lang="stylus" scoped>
-.page-map-index
-  display flex
-  flex-direction column
-  align-items center
-  justify-content flex-start !important
-  position absolute
-  height 100vh
-  width 100vw
-  overflow hidden
-  background url('~@/assets//image/map_index_bg.png') center center no-repeat
-  background-size cover
-.video
-  z-index 20
-  height 100%
-  width 100%
-  object-fit cover
-.video-close
-  font-size 5vw
-  color white
-  position absolute
-  left 96.5vw
-  top 0
-  z-index 21
-  text-shadow .1vw .1vw .3vw black
-.opening
-  z-index 11
-.slides
-  img
-    z-index 15
-.hidden
-  visibility hidden
-.buttonGroup
-  position absolute
-  left 12vw
-  bottom 5vw
-  display flex
-  width 48vw
-  justify-content space-between
-  z-index 10
-  img
-    cursor pointer
-    height 10vw
-    width 9vw
-    object-position 0 0
-    object-fit cover
-img.lotus
-  right -6vw
-  position absolute
-  bottom -3.5vw
-  width 37vw
-.bg-element
-  left 0
-  top 0
-  position absolute
-  width 100vw
-  height 100vh
-.title-circle
-  width 10vw
-  height 10vw
-  border solid white 0.25vw
-  border-radius 5vw
-  left 17.2vw
-  position absolute
-  top 16.5vw
-  z-index 1
-  &.no-2
-    left 28.5vw
-  &.no-3
-    left 39.8vw
-  &.no-4
-    left 50.8vw
-.slides
-  .menu-links
-    a
-      position absolute
-      z-index 20
-      &.section-1
-        left 15vw
-        top 10vh
-        width 30vw
-        height 15vh
-      &.section-2
-        left 61vw
-        top 20vh
-        width 26vw
-        height 16vh
-      &.section-3
-        left 6.25vw
-        top 53vh
-        width 30vw
-        height 13.42vh
-      &.section-4
-        left 54.7vw
-        top 67vh
-        width 36.7vw
-        height 13.9vh
-  .back
-    position absolute
-    z-index 20
-    left 88.5vw
-    top 6.7vh
-    width 7.66vw
-    height 7.03vh
-    &.section-cover
-      left 46vw
-      top 88vh
-      width 7.76vw
-      height 6.94vh
-@keyframes swipe
-  0%
-    top 0
-    left 0
-  50%
-    top 2vh
-    left 2vw
-  100%
-    top 0
-    left 0
-@keyframes swipe-reverse
-  0%
-    top 0
-    left 0
-  50%
-    top -2vh
-    left -2vw
-  100%
-    top 0
-    left 0
-@keyframes fill
-  0%
-    background rgba(255,255,255,0.5)
-  50%
-    background rgba(255,255,255,0)
-  100%
-    background rgba(255,255,255,0.5)
-@keyframes glow
-  0%
-    opacity 1
-  50%
-    opacity 0.5
-  100%
-    opacity 1
-.swipe
-  animation swipe 15s linear infinite
-.swipe-reverse
-  animation swipe-reverse 10s linear infinite
-.fill
-  animation fill 3s ease-in-out infinite
-.glow
-  animation glow 3s ease-in-out infinite
-.delay-1
-  animation-delay 1s
-.delay-2
-  animation-delay 2s
-.delay-3
-  animation-delay 3s
+.page-map-index {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start !important;
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  background: url('~@/assets//image/map_index_bg.png') center center no-repeat;
+  background-size: cover;
+}
+
+.video {
+  z-index: 20;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+
+.video-close {
+  font-size: 5vw;
+  color: white;
+  position: absolute;
+  left: 96.5vw;
+  top: 0;
+  z-index: 21;
+  text-shadow: 0.1vw 0.1vw 0.3vw black;
+}
+
+.opening {
+  z-index: 11;
+}
+
+.slides {
+  img {
+    z-index: 15;
+  }
+}
+
+.hidden {
+  visibility: hidden;
+}
+
+.buttonGroup {
+  position: absolute;
+  left: 12vw;
+  bottom: 5vw;
+  display: flex;
+  width: 48vw;
+  justify-content: space-between;
+  z-index: 10;
+
+  img {
+    cursor: pointer;
+    height: 10vw;
+    width: 9vw;
+    object-position: 0 0;
+    object-fit: cover;
+  }
+}
+
+img.lotus {
+  right: -6vw;
+  position: absolute;
+  bottom: -3.5vw;
+  width: 37vw;
+}
+
+.bg-element {
+  left: 0;
+  top: 0;
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+}
+
+.title-circle {
+  width: 10vw;
+  height: 10vw;
+  border: solid white 0.25vw;
+  border-radius: 5vw;
+  left: 17.2vw;
+  position: absolute;
+  top: 16.5vw;
+  z-index: 1;
+
+  &.no-2 {
+    left: 28.5vw;
+  }
+
+  &.no-3 {
+    left: 39.8vw;
+  }
+
+  &.no-4 {
+    left: 50.8vw;
+  }
+}
+
+.slides {
+  .menu-links {
+    a {
+      position: absolute;
+      z-index: 20;
+
+      &.section-1 {
+        left: 15vw;
+        top: 10vh;
+        width: 30vw;
+        height: 15vh;
+      }
+
+      &.section-2 {
+        left: 61vw;
+        top: 20vh;
+        width: 26vw;
+        height: 16vh;
+      }
+
+      &.section-3 {
+        left: 6.25vw;
+        top: 53vh;
+        width: 30vw;
+        height: 13.42vh;
+      }
+
+      &.section-4 {
+        left: 54.7vw;
+        top: 67vh;
+        width: 36.7vw;
+        height: 13.9vh;
+      }
+    }
+  }
+
+  .back {
+    position: absolute;
+    z-index: 20;
+    left: 88.5vw;
+    top: 6.7vh;
+    width: 7.66vw;
+    height: 7.03vh;
+
+    &.section-cover {
+      left: 46vw;
+      top: 88vh;
+      width: 7.76vw;
+      height: 6.94vh;
+    }
+  }
+}
+
+@keyframes swipe {
+  0% {
+    top: 0;
+    left: 0;
+  }
+
+  50% {
+    top: 2vh;
+    left: 2vw;
+  }
+
+  100% {
+    top: 0;
+    left: 0;
+  }
+}
+
+@keyframes swipe-reverse {
+  0% {
+    top: 0;
+    left: 0;
+  }
+
+  50% {
+    top: -2vh;
+    left: -2vw;
+  }
+
+  100% {
+    top: 0;
+    left: 0;
+  }
+}
+
+@keyframes fill {
+  0% {
+    background: rgba(255, 255, 255, 0.5);
+  }
+
+  50% {
+    background: rgba(255, 255, 255, 0);
+  }
+
+  100% {
+    background: rgba(255, 255, 255, 0.5);
+  }
+}
+
+@keyframes glow {
+  0% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0.5;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+
+.swipe {
+  animation: swipe 15s linear infinite;
+}
+
+.swipe-reverse {
+  animation: swipe-reverse 10s linear infinite;
+}
+
+.fill {
+  animation: fill 3s ease-in-out infinite;
+}
+
+.glow {
+  animation: glow 3s ease-in-out infinite;
+}
+
+.delay-1 {
+  animation-delay: 1s;
+}
+
+.delay-2 {
+  animation-delay: 2s;
+}
+
+.delay-3 {
+  animation-delay: 3s;
+}
 </style>
